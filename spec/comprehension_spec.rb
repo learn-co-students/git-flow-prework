c2de8c4076f9d7f62a540bc9ef2a9ee841626a27
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-27 03:11:21 +0000

    Done.

--- spec/comprehension_spec.rb
+++ spec/comprehension_spec.rb
@@ -1,7 +1,7 @@
 describe "GIT" do
 # describe "STAGING AND COMMITTING CHANGES:" do
   it "1. What is the syntax for staging a change in index.html if it is in the root dir?" do
-    answer = "?"
+    answer = "git add"
     encoded_answer = "bf338924ddc9ff4eb0c685a794960e0c4ffade2c"
     expect(encode(answer)).to eq(encoded_answer)
   end
